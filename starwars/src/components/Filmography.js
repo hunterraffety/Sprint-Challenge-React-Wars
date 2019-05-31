ad88932045f2797d4f6706735dd115b86bd0e6a7
hsrsbd <hsraffety@gmail.com>
2019-05-31 09:55:55 -0700

    truncated text w react-truncate-text. further styling

--- starwars/src/components/Filmography.js
+++ starwars/src/components/Filmography.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../components/Filmography.scss';
-
+import TextTruncate from 'react-text-truncate';
 class Filmography extends React.Component {
   render() {
     return (
       <div class='single-film'>
         <li class='film-title'>{this.props.filmInState.title}</li>
-        <li className='film-crawl'>{this.props.filmInState.opening_crawl}</li>
+        <TextTruncate
+          className='text-truncate'
+          line={1}
+          truncateText='…'
+          text={this.props.filmInState.opening_crawl}
+          textTruncateChild={
+            <p className='read-more'>
+              <a href='#'>Read more...</a>
+            </p>
+          }
+        />
+        <li className='film-crawl' />
       </div>
     );
   }
