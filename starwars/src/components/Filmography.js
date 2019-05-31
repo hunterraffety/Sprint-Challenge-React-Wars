835373f90e29b0a00d60b14e854931303913939f
hsrsbd <hsraffety@gmail.com>
2019-05-31 09:46:54 -0700

    updated styling.

--- starwars/src/components/Filmography.js
+++ starwars/src/components/Filmography.js
@@ -4,10 +4,10 @@ import '../components/Filmography.scss';
 class Filmography extends React.Component {
   render() {
     return (
-      <ul>
+      <div class='single-film'>
         <li class='film-title'>{this.props.filmInState.title}</li>
         <li className='film-crawl'>{this.props.filmInState.opening_crawl}</li>
-      </ul>
+      </div>
     );
   }
 }
