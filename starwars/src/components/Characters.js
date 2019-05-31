248f76b33483002b828634d7192ddd8113d5170a
hsrsbd <hsraffety@gmail.com>
2019-05-31 08:19:04 -0700

    install node-sass, basic styling for dev. purposes.

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Characters.scss';
 
 class Characters extends React.Component {
   render() {
-    return <div className='char-card'>{this.props.charInState.birth_year}</div>;
+    return <div className='char-card'>{this.props.charInState.name}</div>;
   }
 }
 
